c04ae9ac05f0cd73b9f187c6b2e8566f7fa6b0aa
LAPTOP-EKM3OTKJ\alex <apdeleon29@gmail.com>
2021-03-07 21:47:34 +0800

    End of section4

--- TreasAPI/Controllers/UsersController.cs
+++ TreasAPI/Controllers/UsersController.cs
@@ -8,9 +8,7 @@ using TreasAPI.Entities;
 
 namespace TreasAPI.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
