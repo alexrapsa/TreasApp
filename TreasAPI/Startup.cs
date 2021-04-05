333570a721781d494ba62c7bf4fba67952578326
LAPTOP-EKM3OTKJ\alex <apdeleon29@gmail.com>
2021-04-06 01:25:49 +0800

    End of section 8

--- TreasAPI/Startup.cs
+++ TreasAPI/Startup.cs
@@ -18,6 +18,7 @@ using Microsoft.OpenApi.Models;
 using TreasAPI.Data;
 using TreasAPI.Extensions;
 using TreasAPI.Interfaces;
+using TreasAPI.Middleware;
 using TreasAPI.Services;
 
 namespace TreasAPI
@@ -48,12 +49,7 @@ namespace TreasAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TreasAPI v1"));
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseHttpsRedirection();
 
